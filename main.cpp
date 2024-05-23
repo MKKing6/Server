9b97f444646c3a8b4bcc5473d9cdb38c817f9ad9
MKKing6 <weihaohwang@gmail.com>
2024-05-23 01:25:34 -0400

    Multiple Clients

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@
 #include <ws2tcpip.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <list>
 
 // // Need to link with Ws2_32.lib
 // #pragma comment (lib, "Ws2_32.lib")
@@ -15,20 +16,27 @@
 #define DEFAULT_BUFLEN 512
 #define DEFAULT_PORT "27015"
 
+using namespace std;
+
 int __cdecl main(void) 
 {
     WSADATA wsaData;
     int iResult;
 
     SOCKET ListenSocket = INVALID_SOCKET;
+    list<SOCKET> ClientList;
     SOCKET ClientSocket = INVALID_SOCKET;
 
     struct addrinfo *result = NULL;
     struct addrinfo hints;
 
+    FD_SET ReadSet;
+
     int iSendResult;
     char recvbuf[DEFAULT_BUFLEN];
     int recvbuflen = DEFAULT_BUFLEN;
+
+    const TIMEVAL timeVal = {1, 0};
     
     // Initialize Winsock
     iResult = WSAStartup(MAKEWORD(2,2), &wsaData);
@@ -81,48 +89,83 @@ int __cdecl main(void)
     }
 
     printf("listening\n");
-    // Accept a client socket
-    ClientSocket = accept(ListenSocket, NULL, NULL);
-    if (ClientSocket == INVALID_SOCKET) {
-        printf("accept failed with error: %d\n", WSAGetLastError());
-        closesocket(ListenSocket);
-        WSACleanup();
-        return 1;
-    }
-    printf("stop");
 
-    // No longer need server socket
-    closesocket(ListenSocket);
+    while (true) {
+        FD_ZERO(&ReadSet);
+        FD_SET(ListenSocket, &ReadSet);
+        list<SOCKET>::iterator it;
+        for (it = ClientList.begin(); it != ClientList.end(); ++it) {
+            FD_SET(*it, &ReadSet);
+        }
 
-    // Receive until the peer shuts down the connection
-    do {
+        select(0, &ReadSet, NULL, NULL, &timeVal);
 
-        iResult = recv(ClientSocket, recvbuf, recvbuflen, 0);
-        if (iResult > 0) {
-            recvbuf[iResult] = 0;
-            printf("Bytes received: %d\n%s\n", iResult, recvbuf);
-
-            strcat(recvbuf, " 123");
-        // Echo the buffer back to the sender
-            iSendResult = send( ClientSocket, recvbuf, (int)strlen(recvbuf), 0 );
-            if (iSendResult == SOCKET_ERROR) {
-                printf("send failed with error: %d\n", WSAGetLastError());
-                closesocket(ClientSocket);
-                WSACleanup();
-                return 1;
+        for (it = ClientList.begin(); it != ClientList.end(); ++it) {
+            if (FD_ISSET(*it, &ReadSet)) {
+                iResult = recv(ClientSocket, recvbuf, recvbuflen, 0);
+                if (iResult > 0) {
+                    recvbuf[iResult] = 0;
+                    printf("Bytes received: %d\n%s\n", iResult, recvbuf);
+                    
+                    strcat(recvbuf, " 123");
+                    // Echo the buffer back to the sender
+                    iSendResult = send( ClientSocket, recvbuf, (int)strlen(recvbuf), 0 );
+                    if (iSendResult == SOCKET_ERROR) {
+                        printf("send failed with error: %d\n", WSAGetLastError());
+                        closesocket(ClientSocket);
+                        WSACleanup();
+                        return 1;
+                    }
+                    printf("Bytes sent: %d\n", iSendResult);
+                }
             }
-            printf("Bytes sent: %d\n", iSendResult);
-        }
-        else if (iResult == 0)
-            printf("Connection closing...\n");
-        else  {
-            printf("recv failed with error: %d\n", WSAGetLastError());
-            closesocket(ClientSocket);
-            WSACleanup();
-            return 1;
         }
 
-    } while (iResult > 0);
+        if (FD_ISSET(ListenSocket, &ReadSet)) {
+            // Accept a client socket
+            ClientSocket = accept(ListenSocket, NULL, NULL);
+            if (ClientSocket == INVALID_SOCKET) {
+                printf("accept failed with error: %d\n", WSAGetLastError());
+            }
+            else {
+                ClientList.push_back(ClientSocket);
+                printf("Connected\n");
+            }
+
+            // No longer need server socket
+            //closesocket(ListenSocket);
+
+            // Receive until the peer shuts down the connection
+            // do {
+
+            //     iResult = recv(ClientSocket, recvbuf, recvbuflen, 0);
+            //     if (iResult > 0) {
+            //         recvbuf[iResult] = 0;
+            //         printf("Bytes received: %d\n%s\n", iResult, recvbuf);
+
+            //         strcat(recvbuf, " 123");
+            //     // Echo the buffer back to the sender
+            //         iSendResult = send( ClientSocket, recvbuf, (int)strlen(recvbuf), 0 );
+            //         if (iSendResult == SOCKET_ERROR) {
+            //             printf("send failed with error: %d\n", WSAGetLastError());
+            //             closesocket(ClientSocket);
+            //             WSACleanup();
+            //             return 1;
+            //         }
+            //         printf("Bytes sent: %d\n", iSendResult);
+            //     }
+            //     else if (iResult == 0)
+            //         printf("Connection closing...\n");
+            //     else  {
+            //         printf("recv failed with error: %d\n", WSAGetLastError());
+            //         closesocket(ClientSocket);
+            //         WSACleanup();
+            //         return 1;
+            //     }
+
+            // } while (iResult > 0);
+        }
+    }
 
     // shutdown the connection since we're done
     iResult = shutdown(ClientSocket, SD_SEND);
